2e6ea68cd0792466fa1a395787181f0be0a8ee74
kamangir <arash@kamangir.net>
2025-02-08 15:00:13 -0800

    ++ - kamangir/bolt#746

--- blue_cemetery/README.py
+++ blue_cemetery/README.py
@@ -3,21 +3,41 @@ import os
 from blue_objects import file, README
 
 from blue_cemetery import NAME, VERSION, ICON, REPO_NAME
-from blue_cemetery.list import list_of_experiments
 
 
-items = [
-    "[`{}`](#) [![image]({})]({}) {}".format(
-        experiment_name,
-        experiment.get(
-            "marquee",
-            "https://github.com/kamangir/assets/raw/main/blue-plugin/marquee.png?raw=true",
-        ),
-        experiment["url"],
-        experiment.get("title", ""),
-    )
-    for experiment_name, experiment in list_of_experiments.items()
-]
+items = README.Items(
+    [
+        {
+            "name": "sagesemseg",
+            "title": "A SemSeg (Semantic Segmenter) trained and deployed on AWS Sagemaker.",
+            "url": "./blue_sandbox/sagesemseg/README.md",
+            "marquee": "https://github.com/kamangir/assets/blob/main/blue-sandbox/sagesemseg-predict.png?raw=true",
+        },
+        {
+            "name": "@damages",
+            "title": "Satellite imagery damage assessment workflow",
+            "url": "./blue_sandbox/microsoft_building_damage_assessment/README.md",
+            "marquee": "https://github.com/kamangir/assets/raw/main/blue-sandbox/Maui-Hawaii-fires-Aug-23-ingest-2025-01-10-qqJqhm.png?raw=true",
+        },
+        {
+            "name": "VisuaLyze",
+            "title": 'How about calling it "VisuaLyze"? ... - OpenAI',
+            "url": "./blue_sandbox/VisuaLyze",
+            "marquee": "https://github.com/kamangir/openai-commands/assets/1007567/7c0ed5f7-6941-451c-a17e-504c6adab23f",
+        },
+        {
+            "name": "gpt",
+            "title": "co-authored with ChapGPT.",
+            "url": "./blue_sandbox/cemetery/gpt",
+        },
+        {
+            "name": "code generation",
+            "title": "",
+            "marquee": "https://github.com/kamangir/openai-commands/blob/main/assets/completion_i2i_function.png?raw=true",
+            "url": "./blue_sandbox/cemetery/code_generation",
+        },
+    ]
+)
 
 
 def build():
