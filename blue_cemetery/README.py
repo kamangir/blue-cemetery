c03af07732ebdd53a4a21da41421814ee0abe79c
kamangir <arash@kamangir.net>
2025-03-17 19:02:21 -0700

    doc++ - kamangir/bolt#746 - no-workflow 🪄

--- blue_cemetery/README.py
+++ blue_cemetery/README.py
@@ -10,19 +10,19 @@ items = README.Items(
         {
             "name": "sagesemseg",
             "description": "A SemSeg (Semantic Segmenter) trained and deployed on AWS Sagemaker.",
-            "url": "./blue_sandbox/sagesemseg/README.md",
+            "url": "./blue_sandbox/cemetery/sagesemseg",
             "marquee": "https://github.com/kamangir/assets/blob/main/blue-sandbox/sagesemseg-predict.png?raw=true",
         },
         {
             "name": "@damages",
             "description": "Satellite imagery damage assessment workflow",
-            "url": "./blue_sandbox/microsoft_building_damage_assessment/README.md",
+            "url": "./blue_sandbox/cemetery/microsoft_building_damage_assessment",
             "marquee": "https://github.com/kamangir/assets/raw/main/blue-sandbox/Maui-Hawaii-fires-Aug-23-ingest-2025-01-10-qqJqhm.png?raw=true",
         },
         {
             "name": "VisuaLyze",
             "description": 'How about calling it "VisuaLyze"? ... - OpenAI',
-            "url": "./blue_sandbox/VisuaLyze",
+            "url": "./blue_sandbox/cemetery/VisuaLyze",
             "marquee": "https://github.com/kamangir/openai-commands/assets/1007567/7c0ed5f7-6941-451c-a17e-504c6adab23f",
         },
         {
--- blue_cemetery/__init__.py
+++ blue_cemetery/__init__.py
@@ -4,7 +4,7 @@ ICON = "🪦"
 
 DESCRIPTION = f"{ICON} An AI cemetery."
 
-VERSION = "3.235.1"
+VERSION = "3.236.1"
 
 REPO_NAME = "blue-cemetery"
 
